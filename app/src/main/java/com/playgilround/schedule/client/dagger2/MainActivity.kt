cae02eac6894177ff338eab988bc35e61516072e
hyunwook <c004245@naver.com>
2019-04-22 23:13:42 +0900

    Migrate AndroidX

--- app/src/main/java/com/playgilround/schedule/client/dagger2/MainActivity.kt
+++ app/src/main/java/com/playgilround/schedule/client/dagger2/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.playgilround.schedule.client.dagger2
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.playgilround.schedule.client.dagger2.presenter.MainPresenter
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainPresenter.View {
+
+    @Inject
+    lateinit var presenter: MainPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/playgilround/schedule/client/dagger2/presenter/MainPresenter.kt
@@ -0,0 +1,8 @@
+package com.playgilround.schedule.client.dagger2.presenter
+
+interface MainPresenter {
+
+    interface View {
+
+    }
+}
