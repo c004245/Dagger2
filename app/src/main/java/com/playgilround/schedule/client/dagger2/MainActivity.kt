95915797657b5a5cab90b5a937d65a8ea2fbc7a0
hyunwook <c004245@naver.com>
2019-07-31 10:56:23 +0900

    SearchView 기능 추가

--- app/src/main/java/com/playgilround/schedule/client/dagger2/MainActivity.kt
+++ app/src/main/java/com/playgilround/schedule/client/dagger2/MainActivity.kt
@@ -2,6 +2,8 @@ package com.playgilround.schedule.client.dagger2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.playgilround.schedule.client.dagger2.presenter.MainPresenter
 import dagger.internal.DaggerCollections
@@ -20,6 +22,24 @@ class MainActivity : AppCompatActivity(), MainPresenter.View {
 //        DaggerCoffeeComponent.create().inject(coffeeMaker)
 //        coffeeMaker.brew()
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        val searchView = menu?.findItem(R.id.search_bar)?.actionView as? SearchView
+        searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(s: String?): Boolean {
+                s?.let {
+                    searchGithubUser(s)
+                }
+                return false
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
     }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
