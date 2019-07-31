ba3634fb49c36081a3038be6f25e51c7e48488c2
hyunwook <c004245@naver.com>
2019-07-31 12:01:54 +0900

    UserFragment <-> UserListAdapter 연결

--- app/src/main/java/com/playgilround/schedule/client/dagger2/view/UserFragment.kt
+++ app/src/main/java/com/playgilround/schedule/client/dagger2/view/UserFragment.kt
@@ -1,5 +1,6 @@
 package com.playgilround.schedule.client.dagger2.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,14 +9,22 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.playgilround.schedule.client.dagger2.R
+import com.playgilround.schedule.client.dagger2.model.User
+import com.playgilround.schedule.client.dagger2.view.adapter.UserListAdapter
 
 class UserFragment : Fragment() {
 
+    val items by lazy { ArrayList<User>() }
+
+    val userAdapter by lazy { UserListAdapter(context, items) }
+
+    @SuppressLint("WrongConstant")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_user, container, false) as RecyclerView
         view.apply {
             layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
             setHasFixedSize(true)
+            adapter = userAdapter
         }
         return view
     }
