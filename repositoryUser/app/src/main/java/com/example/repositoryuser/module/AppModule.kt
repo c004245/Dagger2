b283ba10af4a9033c942c25d154044359783226a
hyunwook <c004245@naver.com>
2019-08-02 19:09:31 +0900

    Dagger2 관련된 라이브러리 추가

---
+++ repositoryUser/app/src/main/java/com/example/repositoryuser/module/AppModule.kt
@@ -0,0 +1,10 @@
+package com.example.repositoryuser.module
+
+import android.app.Application
+import dagger.Module
+
+@Module
+class AppModule constructor(private val application: Application) {
+
+
+}
