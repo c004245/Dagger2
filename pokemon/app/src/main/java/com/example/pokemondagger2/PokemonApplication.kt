7c59da3c8cb47aa305b5a0c6981fca14bfd86f69
hyunwook <c004245@naver.com>
2019-08-14 10:33:08 +0900

    Application DispatchInject

---
+++ pokemon/app/src/main/java/com/example/pokemondagger2/PokemonApplication.kt
@@ -0,0 +1,15 @@
+package com.example.pokemondagger2
+
+import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
+
+class PokemonApplication: Application(), HasAndroidInjector {
+
+    @Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
+
+    override fun androidInjector() = androidInjector
+}
