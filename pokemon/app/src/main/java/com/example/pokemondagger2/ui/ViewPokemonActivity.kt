05ab80fa9359917625281e9839ee06adf98003f0
hyunwook <c004245@naver.com>
2019-08-14 11:39:47 +0900

    Adapter 구성 (Recycler)

--- pokemon/app/src/main/java/com/example/pokemondagger2/ui/ViewPokemonActivity.kt
+++ pokemon/app/src/main/java/com/example/pokemondagger2/ui/ViewPokemonActivity.kt
@@ -2,7 +2,10 @@ package com.example.pokemondagger2.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.example.pokemondagger2.R
 import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 class ViewPokemonActivity: AppCompatActivity() {
@@ -13,6 +16,13 @@ class ViewPokemonActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val adapter = PokemonAdapter()
+
+        recycler_view.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        recycler_view.adapter = adapter
+
 
     }
 }
