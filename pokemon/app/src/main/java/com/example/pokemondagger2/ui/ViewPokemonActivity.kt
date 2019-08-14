c432b6fbbba917ed822d26a54ea8b307067859dd
hyunwook <c004245@naver.com>
2019-08-14 11:49:18 +0900

    Kotlin Core 추가

--- pokemon/app/src/main/java/com/example/pokemondagger2/ui/ViewPokemonActivity.kt
+++ pokemon/app/src/main/java/com/example/pokemondagger2/ui/ViewPokemonActivity.kt
@@ -1,14 +1,17 @@
 package com.example.pokemondagger2.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.pokemondagger2.R
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
-class ViewPokemonActivity: AppCompatActivity() {
+class ViewPokemonActivity: AppCompatActivity(), LifecycleOwner {
 
     @Inject
     lateinit var viewModel: ViewPokemonViewModel
@@ -23,6 +26,9 @@ class ViewPokemonActivity: AppCompatActivity() {
         recycler_view.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
         recycler_view.adapter = adapter
 
-
+        viewModel.pokemon.observe(this, Observer {
+            progress_bar.visibility = View.GONE
+            adapter.setItems(it)
+        })
     }
 }
--- pokemon/app/src/main/java/com/example/pokemondagger2/ui/ViewPokemonViewModel.kt
+++ pokemon/app/src/main/java/com/example/pokemondagger2/ui/ViewPokemonViewModel.kt
@@ -3,9 +3,7 @@ package com.example.pokemondagger2.ui
 import com.example.pokemondagger2.data.PokemonRepository
 import javax.inject.Inject
 
-class ViewPokemonViewModel @Inject constructor(
-    private val pokemonRepository: PokemonRepository
-) {
+class ViewPokemonViewModel @Inject constructor(pokemonRepository: PokemonRepository) {
 
-    fun getPokemon() = pokemonRepository.getPokemon()
+    val pokemon = pokemonRepository.getPokemon()
 }
