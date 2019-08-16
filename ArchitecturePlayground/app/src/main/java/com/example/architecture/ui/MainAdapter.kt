a75658a251353a452fa8fd917de7c6bab222a893
hyunwook <c004245@naver.com>
2019-08-16 11:58:54 +0900

    MainAdapter

--- ArchitecturePlayground/app/src/main/java/com/example/architecture/ui/MainAdapter.kt
+++ ArchitecturePlayground/app/src/main/java/com/example/architecture/ui/MainAdapter.kt
@@ -1,15 +1,47 @@
 package com.example.architecture.ui
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.architecture.R
+import com.example.architecture.services.GithubRepoEntity
+import kotlinx.android.synthetic.main.recycler_row.view.*
 
-class MainAdapter(private val listener: (GithubRepoEntity) -> Unit): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class MainAdapter(private val listener: (GithubRepoEntity) -> Unit): RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+
+    private var items: List<GithubRepoEntity> = listOf()
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater: LayoutInflater = LayoutInflater.from(parent.context)
+        val view: View = layoutInflater.inflate(R.layout.recycler_row, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position], listener)
+    }
+
+    fun addAll(items: List<GithubRepoEntity>) {
+        this.items = items
+        notifyDataSetChanged()
+    }
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
         fun bind(
-            reposi
-        )
+            repository: GithubRepoEntity,
+            listener: (GithubRepoEntity) -> Unit) {
+            with(itemView) {
+                main_txt.text = repository.name
+                setOnClickListener {
+                    listener(repository)
+                }
+            }
+        }
     }
 }
-)
