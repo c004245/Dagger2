c267f478b9cae7fc6631b58aeb0fdf30bea16238
hyunwook <c004245@naver.com>
2019-08-16 12:32:42 +0900

    Repository Detail Presenter , Activity

--- ArchitecturePlayground/app/src/main/java/com/example/architecture/ui/details/DetailsPresenter.kt
+++ ArchitecturePlayground/app/src/main/java/com/example/architecture/ui/details/DetailsPresenter.kt
@@ -1,3 +1,28 @@
 package com.example.architecture.ui.details
 
-class
+import com.example.architecture.services.GithubRepoEntity
+import javax.inject.Inject
+
+class DetailsPresenter @Inject constructor(): DetailsContract.Presenter {
+
+    private var view: DetailsContract.View? = null
+    private lateinit var githubRepoEntity: GithubRepoEntity
+
+    override fun attachView(view: DetailsContract.View) {
+        this.view = view
+    }
+
+    override fun detachView() {
+        this.view = null
+    }
+
+    override fun initData(githubRepoEntity: GithubRepoEntity) {
+        this.githubRepoEntity = githubRepoEntity
+        view?.setToolbarTitle(githubRepoEntity.name)
+        view?.setUrlAddress(githubRepoEntity.url)
+    }
+
+    override fun onGoToRepositoryClicked() {
+        view?.startUrl(githubRepoEntity.url)
+    }
+}
