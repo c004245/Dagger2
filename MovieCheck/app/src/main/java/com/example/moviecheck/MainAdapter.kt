831bc2cc27c33294f1f0524eece5f5a900609758
hyunwook <c004245@naver.com>
2019-08-27 14:49:10 +0900

    Inject adapter, presenter Fragment, adapter

--- MovieCheck/app/src/main/java/com/example/moviecheck/MainAdapter.kt
+++ MovieCheck/app/src/main/java/com/example/moviecheck/MainAdapter.kt
@@ -1,11 +1,51 @@
 package com.example.moviecheck
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.get
 import androidx.recyclerview.widget.RecyclerView
+import com.example.moviecheck.model.Movie
+import com.squareup.picasso.Picasso
+import javax.inject.Inject
 
-class MainAdapter: RecyclerView.Adapter() {
+class MainAdapter @Inject constructor(private var picasso: Picasso): RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+
+    lateinit var listMovie: List<Movie>
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        return MainViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        holder.bind(listMovie[position])
+    }
 
 
-    class ViewHolder() {
+    fun setMovies(movies: List<Movie>) {
+        this.listMovie = movies
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+        return if (listMovie == null) 0 else listMovie.size
 
     }
+
+    inner class MainViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        private var movieTitle: TextView = itemView.findViewById(R.id.textViewMovieTitle)
+        private var moviePoster: ImageView = itemView.findViewById(R.id.imageViewMoviePoster)
+
+        fun bind(movie: Movie) {
+           movieTitle.text = movie.title
+            picasso.load("https://image.tmdb.org/t/p/w342" + movie.posterPath)
+                .centerCrop()
+                .fit()
+                .into(moviePoster)
+       }
+   }
+
 }
--- MovieCheck/app/src/main/java/com/example/moviecheck/view/MainFragment.kt
+++ MovieCheck/app/src/main/java/com/example/moviecheck/view/MainFragment.kt
@@ -5,13 +5,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moviecheck.MainAdapter
+import com.example.moviecheck.R
+import com.example.moviecheck.model.Movie
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.fragment_main.*
 import javax.inject.Inject
 
 class MainFragment @Inject constructor(): DaggerFragment(), MainContract.View {
 
+    @Inject
+    lateinit var adapter: RecyclerView.Adapter<MainAdapter.MainViewHolder>
+
+    @Inject
+    lateinit var presenter: MainContract.Presenter
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout)
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = adapter
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        presenter.setView(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.destroyView()
+    }
+
+    override fun onMoviesLoaded(movies: List<Movie>) {
+        (adapter as MainAdapter).setMovies(movies)
     }
 
 }
