4fa62d1a54d39ed382ceeea015d9de29ec134fec
hyunwook <c004245@naver.com>
2019-08-27 10:48:00 +0900

    Movie List Dagger2 Project 생성

--- ArchitecturePlayground/app/src/main/java/com/example/architecture/App.kt
+++ ArchitecturePlayground/app/src/main/java/com/example/architecture/App.kt
@@ -1,14 +1,10 @@
 package com.example.architecture
 
 import com.example.architecture.di.DaggerAndroidPlaygroundComponent
-import com.example.architecture.services.GithubService
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
-import javax.inject.Inject
 
 class App: DaggerApplication() {
-
-    @Inject lateinit var githubService: GithubService
 
     override fun applicationInjector(): AndroidInjector<App> {
         return DaggerAndroidPlaygroundComponent.builder()
