fff11b20df6d27596483b9850412ec83c663b360
hyunwook <c004245@naver.com>
2019-08-27 11:28:20 +0900

    Provide Retrofit repl

--- MovieCheck/app/src/main/java/com/example/moviecheck/ApplicationModule.kt
+++ MovieCheck/app/src/main/java/com/example/moviecheck/ApplicationModule.kt
@@ -1,19 +1,37 @@
 package com.example.moviecheck
 
+import android.app.Application
 import android.content.Context
 import com.squareup.picasso.Picasso
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
-open class ApplicationModule {
+abstract class ApplicationModule {
 
     companion object {
+
         @Singleton
         @Provides
         fun providePicasso(context: Context): Picasso {
             return Picasso.Builder(context).build()
         }
+
+        @Singleton
+        @Provides
+        fun provideRetrofit(): Retrofit {
+            return Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BuildConfig.BASE_URL)
+                .build()
+        }
     }
+
+    @Singleton
+    @Binds
+    abstract fun bindContext(application: Application): Context
 }
