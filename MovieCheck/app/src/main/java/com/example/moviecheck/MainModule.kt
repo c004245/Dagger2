0fa8866eb4f215457ba5883365ef3064c7dfa47d
hyunwook <c004245@naver.com>
2019-08-27 18:40:57 +0900

    MainModule

--- MovieCheck/app/src/main/java/com/example/moviecheck/MainModule.kt
+++ MovieCheck/app/src/main/java/com/example/moviecheck/MainModule.kt
@@ -1,46 +1,46 @@
 package com.example.moviecheck
 
-import android.graphics.Movie
 import androidx.recyclerview.widget.RecyclerView
+import com.example.moviecheck.di.ActivityScoped
+import com.example.moviecheck.di.FragmentScoped
+import com.example.moviecheck.model.Movie
+import com.example.moviecheck.model.MovieApi
+import com.example.moviecheck.source.MovieRemoteDataSource
 import com.example.moviecheck.view.MainFragment
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.android.ContributesAndroidInjector
 import retrofit2.Retrofit
-import javax.sql.DataSource
+import com.example.moviecheck.source.DataSource
+import retrofit2.create
+
 
 @Module
-open class MainModule {
+abstract class MainModule {
 
     @FragmentScoped
     @ContributesAndroidInjector
-    open fun provideMainFragment(): MainFragment {}
+    abstract fun provideMainFragment(): MainFragment
 
     @ActivityScoped
     @Binds
-    open fun provideMainPresenter(mainPresenter: MainPresenter) {
-
-    }
+    abstract fun provideMainPresenter(mainPresenter: MainPresenter)
 
     @ActivityScoped
     @Binds
-    open fun provideMainAdapter(mainAdapter: MainAdapter): RecyclerView.Adapter<MainAdapter.ViewHolder> {
-
-    }
+    abstract fun provideMainAdapter(mainAdapter: MainAdapter): RecyclerView.Adapter<MainAdapter.MainViewHolder>
 
     @ActivityScoped
     @Binds
-    open fun provideMovieDataSource(movieRemoteDataSource: MovieRemoteDataSource): DataSource<Movie> {
-
-    }
+    abstract fun provideMovieDataSource(movieRemoteDataSource: MovieRemoteDataSource): DataSource<Movie>
 
 
     companion object {
         @ActivityScoped
         @Provides
-        fun provideMovidApi(retrofit: Retrofit): MediaRemoteDataSource.MovieApi {
-            return retrofit.create(MovieRemoteDataSource.MovieApi.class)
+        fun provideMovieApi(retrofit: Retrofit): MovieApi {
+            return retrofit.create(MovieApi::class.java)
         }
     }
 }
--- MovieCheck/app/src/main/java/com/example/moviecheck/MoviesApplication.kt
+++ MovieCheck/app/src/main/java/com/example/moviecheck/MoviesApplication.kt
@@ -1,5 +1,6 @@
 package com.example.moviecheck
 
+import com.example.moviecheck.di.AppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
